7507114a62abda1d74a36b2e07d6d15fdf34cacf
Tushar Bhardwaj <redburg035@gmail.com>
2026-04-21 14:05:09 +0530

    Add matchRoutes.js

---
+++ ai-shortlist-system/backend/routes/matchRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const { basicMatch, aiShortlist } = require("../controllers/matchController");
+
+router.post("/", basicMatch);
+router.post("/ai/shortlist", aiShortlist);
+
+module.exports = router;
